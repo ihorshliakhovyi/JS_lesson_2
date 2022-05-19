8f1a03b7313ed214fe2a71cc184fc80dd2b9693d
Ihor Shliakhovyi <ihorshliakhovyi@gmail.com>
2022-05-20 01:48:02 +0300

    Task_6

--- js/Index6.js
+++ js/Index6.js
@@ -1,1 +1,20 @@
 "use strict";
+
+let input;
+const numbersArray = [];
+let total = 0;
+
+do {
+  input = prompt("Input a number to count");
+
+  numbersArray.push(input);
+} while (input != null);
+
+console.log(numbersArray);
+for (let i = 0; i < numbersArray.length - 1; i += 1) {
+  total = total + parseInt(numbersArray[i]);
+}
+
+
+
+alert(total);
